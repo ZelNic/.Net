a82e1e985b3afa6ecfbde5900fda439492a05a48
Nikolay <128342772+ZelNic@users.noreply.github.com>
2023-11-10 11:15:59 +0500

    1

---
+++ Task_1/Task_1/DateByMachine.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_1
+{
+    public class DateByMachine
+    {
+        public int Id { get; set; }
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+    }
+}
