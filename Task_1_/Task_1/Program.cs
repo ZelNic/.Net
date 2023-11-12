71efb78d57ae3880f5374dec4d54ddafff752ae4
Nikolay <128342772+ZelNic@users.noreply.github.com>
2023-11-12 16:02:38 +0500

    +++

---
+++ Task_1_/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace Task_1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            DataBase dataBase = new DataBase();
+
+
+            Application.Run(new Map(dataBase));
+        }
+    }
+}
